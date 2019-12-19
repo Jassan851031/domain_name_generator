00525e997c12d0e5d879352bce5d79daa53c2bc2
jassan leyva <jassan851031@gmail.com>
2019-12-19 09:58:19 -0300

    domain_generator

--- src/js/index.js
+++ src/js/index.js
@@ -8,4 +8,23 @@ import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
+
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    //console.log(pronoun[i]);
+    for (let a = 0; a < adj.length; a++) {
+      //console.log(adj[a]);
+      for (let b = 0; b < noun.length; b++) {
+        //console.log(noun[b]);
+        console.log(pronoun[i] + adj[a] + noun[b] + ".com");
+        let text = document.createTextNode(
+          pronoun[i] + adj[a] + noun[b] + ".com"
+        );
+        document.getElementById("random").appendChild(text);
+      }
+    }
+  }
 };
